e4b19d386535596130d71e491ed0ac45fc1996ea
Tim Molter <tim.molter@gmail.com>
2011-12-27 11:10:43 +0100

    changed package name

--- src/com/xeiam/hellowebapp/HelloServlet.java
+++ src/com/xeiam/hellowebapp/HelloServlet.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.hello;
+package com.xeiam.hellowebapp;
 
 import java.io.IOException;
 
